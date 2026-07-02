bd109306317b6df546a07ab33bd74c7dfbcf9728
Lazarpanovic <lazarpanovic@yahoo.com>
2026-07-02 20:46:18 +0200

    Add burger menu

--- script.js
+++ script.js
@@ -43,3 +43,21 @@ petopiaSlides.forEach((slide, index) => {
 
 updatePetopiaSlider(activeIndex);
 startPetopiaAutoSlider();
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links a");
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
+
+  navItems.forEach((link) => {
+    link.addEventListener("click", () => {
+      menuToggle.classList.remove("active");
+      navLinks.classList.remove("active");
+    });
+  });
+}
